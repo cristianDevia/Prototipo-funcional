29a19c51bc927a6e49be1927856c3de5c6d4eb61
cristianDevia <c.devia8@gmail.com>
2018-05-18 23:46:30 -0500

    Clase Test
    test

---
+++ src/prototipo/mundo/Test.java
@@ -0,0 +1,26 @@
+package prototipo.mundo;
+
+import java.io.File;
+
+public class Test {
+
+	public static void main(String[] args) {
+	
+		
+		Proyecto proyecto = new Proyecto();
+		
+		try {
+			File f = new File("");
+			proyecto.registrarPropuesta( new PropuestaGrado("P1", f));
+			
+			System.out.println(proyecto.consultarNombre());
+		} 
+		catch (Exception e) 
+		{
+			
+			e.printStackTrace();
+		}
+
+	}
+
+}
